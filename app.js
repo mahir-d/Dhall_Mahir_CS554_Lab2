8f72e4b8708cef2c9e98cba27715530f44e80991
Mahir <mdhall@stevens.edu>
2020-02-11 12:24:34 -0500

    added gulp file and css

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 
 
+
 app.use(express.static('public'));
 
 
-app.get("/", async (req, res) => {
-    res.redirect('main.html');
+app.use("/", async (req, res) => {
+    res.redirect("main.html");
 
 
 })
@@ -16,8 +17,8 @@ app.use("*", async (req, res) => {
     //page 404 not found
 })
 
-app.listen(3000, () => {
+app.listen(4000, () => {
     console.log('server started');
-    console.log("Routes will be running on http://localhost:3000");
+    console.log("Routes will be running on http://localhost:4000");
 
 });
