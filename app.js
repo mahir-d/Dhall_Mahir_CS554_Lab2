e655d897c9601a106bdf62b0879e4cba3c6733c2
Mahir <mdhall@stevens.edu>
2020-02-12 00:38:52 -0500

    Final commit before upload

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.use("/", async (req, res) => {
+app.get("/", async (req, res) => {
     res.redirect("main.html");
 
 
@@ -15,10 +15,11 @@ app.use("/", async (req, res) => {
 
 app.use("*", async (req, res) => {
     //page 404 not found
+    res.status(404).send({ "Error": "Page not found" });
 })
 
-app.listen(4000, () => {
+app.listen(3000, () => {
     console.log('server started');
-    console.log("Routes will be running on http://localhost:4000");
+    console.log("Routes will be running on http://localhost:3000");
 
 });
